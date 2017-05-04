34a28bc74ae3c21572253acf9823eced01697a87
Ryan <rcola45@hotmail.com>
2017-05-04 15:37:33 -0300

    Added share button

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -1,5 +1,5 @@
 package com.acme.csci3130;
-
+import android.content.Intent;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -27,8 +27,17 @@ public class MainActivity extends Activity {
         display_text.setText(input_text.getEditableText().toString());
     }
 
-    public void resetText(View v){
+    public void resetText(View v) {
         display_text.setText("");
         input_text.setText("");
+    }
+
+    public void shareClick(View v){
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        String shareBody = display_text.getText().toString();
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Shared via CS3130");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+        startActivity(Intent.createChooser(sharingIntent, "Share via"));
     }
 }
