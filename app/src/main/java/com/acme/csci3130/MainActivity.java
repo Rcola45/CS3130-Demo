46ead8974a7ac84b8d1ccb62b6103cf11b2e948d
Ryan Colaiacovo <rcola45@hotmail.com>
2017-05-04 12:54:38 -0300

    Main functionality complete. Layout redone to center items. @strings file updated to include submit button label.

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,12 +2,28 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
+
+    private Button submit_button;
+    private EditText input_text;
+    private TextView display_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        input_text = (EditText) findViewById(R.id.inputText_textbox) ;
+        display_text = (TextView) findViewById(R.id.displayText_label) ;
+
+    }
+
+    public void changeLabel(View v){
+        display_text.setText(input_text.getEditableText().toString());
     }
 }
